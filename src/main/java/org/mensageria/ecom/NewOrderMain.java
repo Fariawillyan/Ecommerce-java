c80604912ec2a848cb5f188a66ae7d6c1b9edfe6
Willyan Faria <fariawillyan@gmail.com>
2022-03-31 08:59:55 -0300

    producer

--- src/main/java/org/mensageria/ecom/NewOrderMain.java
+++ src/main/java/org/mensageria/ecom/NewOrderMain.java
@@ -2,14 +2,25 @@ package org.mensageria.ecom;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 
         var producer = new KafkaProducer<String, String>(properties());
+        var value = "23154, 54561, 534563";
+        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
+        producer.send(record, (data, ex) ->{
+            if(ex != null){
+                ex.printStackTrace();
+                return;
+            }
+            System.out.println("sucess sending " + data.topic() + "::: partition " + "/" + data.partition() + "/ offset " + data.offset() + "/timestamp " + data.timestamp());
+        }).get();
     }
 
     private static Properties properties() {
