8e043bdcd4f5e3817fb7c41203b9b2fdc3b37ece
Willyan Faria <fariawillyan@gmail.com>
2022-04-04 08:54:39 -0300

    consumer

--- src/main/java/org/mensageria/ecom/NewOrderMain.java
+++ src/main/java/org/mensageria/ecom/NewOrderMain.java
@@ -1,5 +1,6 @@
 package org.mensageria.ecom;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -14,13 +15,19 @@ public class NewOrderMain {
         var producer = new KafkaProducer<String, String>(properties());
         var value = "23154, 54561, 534563";
         var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-        producer.send(record, (data, ex) ->{
-            if(ex != null){
+        Callback callback = (data, ex) -> {
+            if (ex != null) {
                 ex.printStackTrace();
                 return;
             }
             System.out.println("sucess sending " + data.topic() + "::: partition " + "/" + data.partition() + "/ offset " + data.offset() + "/timestamp " + data.timestamp());
-        }).get();
+        };
+
+        var email = "Thank you for your order! We are processing your order!";
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+        producer.send(record, callback).get();
+        producer.send(emailRecord, callback).get();
+
     }
 
     private static Properties properties() {
